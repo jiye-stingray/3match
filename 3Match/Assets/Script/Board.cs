6fc86172ae38214f39f9495586ddf2710cb3aba0
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-11-29 21:02:20 +0900

    :sparkles: ㅣ board tile set

--- 3Match/Assets/Script/Board.cs
+++ 3Match/Assets/Script/Board.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Board Instance { get; private set; }
+
+    public Row[] _rows;
+
+    public Tile[,] Tiles { get; private set; }
+
+    public int Width => Tiles.GetLength(0);
+    public int Height => Tiles.GetLength(1);
+
+    private void Awake() => Instance = this;
+
+    private void Start()
     {
-        
+        Tiles = new Tile[_rows.Max(row => row._tiles.Length), _rows.Length];
+
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                Tiles[x, y] = _rows[y]._tiles[x];
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
