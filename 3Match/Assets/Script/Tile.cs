d420e32d0868e8781af2bf7ab661ec605988329f
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-12-01 09:41:25 +0900

    :sparkles: ㅣ Tile cs add Item

--- 3Match/Assets/Script/Tile.cs
+++ 3Match/Assets/Script/Tile.cs
@@ -8,7 +8,17 @@ public class Tile : MonoBehaviour
     private int x; public int X { get { return x; } set { x = value; } }
     private int y; public int Y { get { return y; } set { y = value; } }
 
-    public Item _item;
+    private Item _item;
+    public Item Item 
+    { 
+        get => _item; 
+        set { 
+            if (_item == value) return; 
+            _item = value;
+            _icon.sprite = _item._sprite;
+        }
+    }
+
     public Image _icon;
     public Button _button;
 }
