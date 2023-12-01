e83307c0475b23bc0f7ee40a14980efcd1bd4519
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-12-01 09:41:51 +0900

    :art: ㅣ Item value change

--- 3Match/Assets/Script/Item.cs
+++ 3Match/Assets/Script/Item.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Match-3/Item")]
 public class Item : ScriptableObject 
 {
-    private int _value; public int Value { get { return _value; } set { _value = value; } }
+    public int _value; 
     public Sprite _sprite;
 }
